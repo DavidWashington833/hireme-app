3d815fdfefe44c877e27564eba406a62a3dd64d5
David <davidwashington833@gmail.com>
2018-03-17 15:21:54 -0300

    Criando page login e começando a page cadastro

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -2,12 +2,16 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: 'page-login',
+  templateUrl: 'login.html'
 })
-export class HomePage {
+export class LoginPage {
 
   constructor(public navCtrl: NavController) {
+
+  }
+
+  logar() {
 
   }
 
