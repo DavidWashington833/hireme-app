e5b18378254f83ef69e45f4b18046e6ac2a12fe9
David <davidwashington833@gmail.com>
2018-04-13 21:50:13 -0300

    Refatorando app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,58 +1,47 @@
-import { MapaPage } from './../pages/mapa/mapa';
 import { Component, ViewChild } from '@angular/core';
-import { Platform, Nav, MenuController, NavController } from 'ionic-angular';
+import { Platform, Nav, MenuController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { MapaPage } from './../pages/mapa/mapa';
 import { LoginPage } from '../pages/login/login';
 import { CadastroPrestadorPage } from '../pages/cadastro-prestador/cadastro-prestador';
 import { RecuperacaoDeSenhaPage } from '../pages/recuperacao-de-senha/recuperacao-de-senha';
 import { CadastroServicoPage } from '../pages/cadastro-servico/cadastro-servico';
 import { ServicosPage } from '../pages/servicos/servicos';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class AppComponent {
-  @ViewChild(Nav) nav: Nav;
-  pages: Array<{title: string, component: any}>;
-  rootPage:any = LoginPage;
+  @ViewChild(Nav)
+  private _nav: Nav;
+  private _pages: Array<{title: string, component: any}>;
+  private _rootPage: any = LoginPage;
 
   constructor(
-    private platform: Platform,
-    private statusBar: StatusBar,
-    private splashScreen: SplashScreen,
+    private _platform: Platform,
+    private _statusBar: StatusBar,
+    private _splashScreen: SplashScreen,
     private _menuCtrl: MenuController
   ) {
-    // used for an example of ngFor and navigation
-    this.pages = [
+    this._pages = [
       { title: 'Serviços', component: ServicosPage.name },
       { title: 'Mapa', component: MapaPage.name },
       { title: 'Cadastrar Prestador', component: CadastroPrestadorPage.name },
       { title: 'Cadastrar Servico', component: CadastroServicoPage.name },
       { title: 'Sair', component: LoginPage }
     ];
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
-    });
-  }
-  initializeApp() {
-    this.platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
-  }
-  openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
   }
 
-  sair() {
-    this.nav.setRoot(LoginPage.name);
+  initializeApp() {
+    this._platform.ready().then(() => {
+      this._statusBar.styleDefault();
+      this._splashScreen.hide();
+    });
+  }
+
+  openPage(page) {
+    this._nav.setRoot(page.component);
   }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { IonicErrorHandler } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
-import { LoginPage } from '../../pages/login/login';
 
 @NgModule({
   imports: [
