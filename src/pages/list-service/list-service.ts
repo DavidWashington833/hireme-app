852761f32207724147a1c4619482f7d620144bbb
David <davidwashington833@gmail.com>
2018-05-27 15:15:02 -0300

    Listando serviços do prestador

--- src/pages/list-service/list-service.ts
+++ src/pages/list-service/list-service.ts
@@ -1,7 +1,10 @@
+import { ServicoProvider } from './../../providers/servico/servico';
 import { RegisterServicePage } from '../register-service/register-service';
 import { DetailServicePage } from '../detail-service/detail-service';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ResponseProvider } from '../../models/ResponseProvider';
+import { ResponseService } from '../../models/ResponseService';
 
 @IonicPage()
 @Component({
@@ -9,13 +12,30 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'list-service.html',
 })
 export class ListServicePage {
+  private services: Array<ResponseService> = new Array<ResponseService>();
 
   constructor(
-    private _navCtrl: NavController
+    private _navCtrl: NavController,
+    private _servicoProvider: ServicoProvider
   ) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ListServicePage');
+
+    const responseProvider: ResponseProvider =
+      JSON.parse(localStorage.getItem('provider'));
+
+    const id = responseProvider.idPrestador.toString();
+
+    this._servicoProvider
+      .getForIdPrestador(id)
+      .subscribe(
+        res => {
+          this.services = res;
+          console.log(res)
+        },
+        err => console.log(err)
+      );
   }
 
   detalheServico() {
