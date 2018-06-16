b3d44647df246fcdc58371f0b19d52bac01fa8e0
David Washington <davidwashington833@gmail.com>
2018-06-16 11:09:02 -0300

    Refatorando AppModule e SharedModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,23 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { Geolocation } from '@ionic-native/geolocation';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicErrorHandler, Form } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { IonicStorageModule } from '@ionic/storage';
+import { AgmCoreModule, GoogleMapsAPIWrapper } from '@agm/core';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBeYIszBebgBTI-HAlw0uXXRMo25gohQv4'
+    }),
+    IonicStorageModule.forRoot({
+      name: 'localdb',
+      driverOrder: ['indexeddb', 'sqlite', 'websql']
+    })
   ],
   providers: [
+    Geolocation,
+    GoogleMapsAPIWrapper,
     Form,
     {
       provide: ErrorHandler,
       useClass: IonicErrorHandler
-    },
+    }
   ]
 })
 export class SharedModule {}
--- src/providers/providers.module.ts
+++ src/providers/providers.module.ts
@@ -2,16 +2,32 @@ import { NgModule } from '@angular/core';
 import { UsuarioProvider } from './usuario/usuario';
 import { AlertProvider } from './alert/alert';
 import { GoogleMapsAPIWrapper } from '@agm/core';
+import { LoadingProvider } from './loading/loading';
+import { EnderecoProvider } from './endereco/endereco';
+import { PrestadorProvider } from './prestador/prestador';
+import { LoginProvider } from './login/login';
+import { GMapsServiceProvider } from './g-maps-service/g-maps-service';
+import { DadosBancariosProvider } from './dados-bancarios/dados-bancarios';
+import { ServicoProvider } from './servico/servico';
+import { CategoriaProvider } from './categoria/categoria';
+import { PedidoProvider } from './pedido/pedido';
 
 @NgModule({
-	declarations: [
-    UsuarioProvider,
-    AlertProvider
-  ],
-	imports: [],
-	exports: [
+  declarations: [],
+  providers: [
     UsuarioProvider,
     AlertProvider,
+    UsuarioProvider,
+    AlertProvider,
+    LoadingProvider,
+    EnderecoProvider,
+    PrestadorProvider,
+    LoginProvider,
+    GMapsServiceProvider,
+    DadosBancariosProvider,
+    ServicoProvider,
+    CategoriaProvider,
+    PedidoProvider
   ]
 })
 export class ProvidersModule {}
