4870394ac075d8850c288bb796faf8856a57fbe8
Breno Cupertino <breno_cupertino@live.com>
2018-06-18 22:01:39 -0300

    Nova ui

--- src/app/main.ts
+++ src/app/main.ts
@@ -3,7 +3,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 
 export function getBaseUrl() {
-  return 'http://localhost:8100/';
+  // return 'http://localhost:8100/';
+  return 'http://54.187.197.222:3000/';
 }
 
 const providers = [
